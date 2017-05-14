1ea03ef3c6c6808a4739f07672f0d36c8390e62f
Owain Williams <owain@andersdigital.com>
2017-05-14 02:06:56 +0100

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Rachael',
-        text: 'Herrow'
-    });
 });
 
 socket.on('disconnect', function() {
